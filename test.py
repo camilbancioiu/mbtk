6565a2554b7004156305f2b96ae3d87355bac0ae
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2019-02-08 16:17:18 +0200

    Create main test script outside the mbff package

---
+++ test.py
@@ -0,0 +1,9 @@
+import unittest
+
+from mbff.tests.datasetmatrix import TestDatasetMatrix
+from mbff.tests.source_rcv1v2 import TestRCV1v2DatasetSource
+from mbff.tests.experimentaldataset import TestExperimentalDataset
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
