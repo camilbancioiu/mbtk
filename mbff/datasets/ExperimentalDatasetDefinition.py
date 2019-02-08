c86f248c99366b5c60651fdd1561020b1e53c16f
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2019-02-08 18:26:04 +0200

    Remove forgotten classes at the end of ExperimentalDatasetDefinition.py

--- mbff/datasets/ExperimentalDatasetDefinition.py
+++ mbff/datasets/ExperimentalDatasetDefinition.py
@@ -108,20 +108,3 @@ class ExperimentalDatasetDefinition():
             print('{}: Folder {} has been unlocked ({}).'.format(self.name, folder, lock_type))
 
 
-class ExperimentalDataset():
-    def __init__(self, definition):
-        self.definition = definition
-        self.matrix = None
-        self.matrix_train = None
-        self.matrix_test = None
-        self.total_row_count = 0
-        self.train_rows = None
-        self.test_rows = None
-
-
-class ExperimentalDatasetError(Exception):
-    def __init__(self, exds_definition, message):
-        self.exds_definition = exds_definition
-        self.message = message
-        super().__init__(self.message)
-
