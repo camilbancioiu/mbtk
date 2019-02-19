72ba8c771f9419c70750727e60dd8e7118ed35e6
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2019-02-19 19:42:18 +0200

    Implement class BinarySyntheticDatasetSource and write tests for it

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import unittest
 
 from mbff_tests.DatasetMatrix import TestDatasetMatrix
 from mbff_tests.RCV1v2DatasetSource import TestRCV1v2DatasetSource
+from mbff_tests.BinarySyntheticDatasetSource import TestBinarySyntheticDatasetSource
 from mbff_tests.ExperimentalDataset import TestExperimentalDataset
 
 if __name__ == '__main__':
