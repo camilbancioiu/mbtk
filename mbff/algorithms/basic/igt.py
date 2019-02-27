4300f9d7c4cef815e38ce5e2d3892e09e3b8b22d
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2019-02-27 11:06:25 +0200

    Add stubs for algorithms and an untested implementation of IGt

---
+++ mbff/algorithms/basic/igt.py
@@ -0,0 +1,15 @@
+import operator
+
+import mbff.infotheory as infotheory
+
+def algorithm_IGt__binary(datasetmatrix, parameters):
+    Q = parameters['Q']
+    objective_vector = datasetmatrix.get_column_Y(parameters['objective_index'])
+    IG_per_feature = []
+    for feature_index in range(feature_count):
+        feature_vector = datasetmatrix.get_column_X(feature_index)
+        feature_IG = infotheory.MI__binary(feature_vector, objective_vector)
+        IG_per_feature.append((feature_index, feature_IG))
+
+    sorted_IG_per_feature = sorted(ig, key=operator.itemgetter(1), reverse=True)
+    return [pair[0] for pair in sorted_IG_per_feature[0:Q]]
