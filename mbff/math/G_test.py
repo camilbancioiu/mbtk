8fe30513ede6c23b52212f61b2e371aa60338740
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2019-05-27 14:14:37 +0300

    Implement G_value__unoptimized_with_cMI()

--- mbff/math/G_test.py
+++ mbff/math/G_test.py
@@ -26,6 +26,28 @@ def G_value__unoptimized(X, Y, Z=None):
 
 
 
+def G_value__unoptimized_with_cMI(X, Y, Z=None):
+    # If Z is none, use the Universe as the conditioning variable.
+    if Z is None:
+        # Raise an exception if one of the Variables has a different number of
+        # instances than the rest, except Z.
+        validate_variable_instances_lengths([X, Y])
+        Z = Omega(len(X.instances))
+    else:
+        # Raise an exception if one of the Variables has a different number of
+        # instances than the rest.
+        validate_variable_instances_lengths([X, Y, Z])
+
+    N = len(X.instances)
+    DF = calculate_degrees_of_freedom(X, Y)
+
+    (PrXYcZ, PrXcZ, PrYcZ, PrZ) = calculate_pmf_for_cmi(X, Y, Z)
+    cMI = conditional_mutual_information(PrXYcZ, PrXcZ, PrYcZ, PrZ, base='e')
+    G = 2 * N * cMI
+    return (G, cMI)
+
+
+
 def calculate_degrees_of_freedom(X, Y):
     return (len(X.values) - 1) * (len(Y.values) - 1)
 
