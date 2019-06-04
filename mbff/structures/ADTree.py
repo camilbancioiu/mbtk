bc1a9a1ffe2084ed58db698db025757005a0c920
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2019-06-04 18:37:12 +0300

    Further implementation of the AD-tree

--- mbff/structures/ADTree.py
+++ mbff/structures/ADTree.py
@@ -1,14 +1,115 @@
+import collections
+
+
 class ADTree:
     
-    def __init__(self, datasetmatrix):
-        self.Root = None
+    def __init__(self, matrix, column_values):
+        self.matrix = matrix
+        self.column_values = column_values
+        self.root = ADNode(self, -1, '*', row_selection=None)
+
+
+    def query(self, values, given={}):
+        pass
+
+    
+    def query_count(self, values, given={}, query_node=None):
+        if query_node is None:
+            query_node = self.root
+
+        if len(values) == 0:
+            return query_node.count
+
+        column_indices = sorted(list(values.keys()))
+        column_index = column_indices[0]
+        value = values[column_index]
+
+        vary = query_node.Vary_children[column_index]
+        node = vary.get_AD_child_with_value(value)
+        
 
 
 
 class ADNode:
-    pass
+
+    def __init__(self, tree, column_index, value, row_selection=None):
+        self.tree = tree
+        self.row_selection = row_selection
+        if self.row_selection is None:
+            self.count = self.tree.matrix.get_shape()[0]
+        else:
+            self.count = len(self.row_selection)
+        self.column_index = column_index
+        self.value = value
+        self.Vary_children = []
+
+        self.create_Vary_children()
+
+
+    def create_Vary_children(self):
+        column_count = self.tree.matrix.get_shape()[1]
+        for column_index in range(self.column_index + 1, column_count):
+            varyNode = VaryNode(self.tree, column_index, self.row_selection)
+            self.Vary_children.append(varyNode)
 
 
 
 class VaryNode:
-    pass
+
+    def __init__(self, tree, column_index, row_selection=None):
+        self.tree = tree
+        self.row_subselections = None
+        self.row_selection = row_selection
+        self.column_index = column_index
+        self.AD_children = []
+
+        self.create_row_subselections_by_value()
+        self.values = self.tree.column_values[column_index]
+        self.most_common_value = self.discoverMCV()
+        self.create_AD_children()
+
+
+    def create_AD_children(self):
+        for value in self.values:
+            row_selection = self.row_subselections[value]
+            adNode = None
+            if len(row_selection) > 0 and value != self.most_common_value:
+                adNode = ADNode(self.tree, self.column_index, value, row_selection)
+            self.AD_children.append(adNode)
+
+
+    def create_row_subselections_by_value(self):
+        """
+        Group the rows of the self.tree.matrix by their value in the first column,
+        but regardless of the rest of the columns. Only iterates over the rows
+        found in row_selection.
+        """
+
+        self.row_subselections = collections.defaultdict(list)
+
+        if self.row_selection is None:
+            # If row_selection isn't set yet, initialize it to cover all the
+            # rows.
+            row_count = self.tree.matrix.get_shape()[0]
+            self.row_selection = range(0, row_count)
+
+        # Iterate over the rows in self.row_selection, putting each row_index
+        # into a list corresponding to its value, in the self.row_subselections
+        # dictionary.
+        for row_index in self.row_selection:
+            value = self.tree.matrix[row_index, self.column_index]
+            self.row_subselections[value].append(row_index)
+
+
+    def get_AD_child_with_value(self, value):
+        for child in self.AD_children:
+            if child is None:
+                continue
+            if child.value == value:
+                return child
+        return None
+
+
+    def discoverMCV(self):
+        return max(self.values, key=lambda v: len(self.row_subselections[v])) 
+
--- mbff_tests/TestADTree.py
+++ mbff_tests/TestADTree.py
@@ -3,85 +3,220 @@ import numpy
 import scipy
 
 from mbff.dataset.DatasetMatrix import DatasetMatrix
-from mbff.structures.ADTree import ADTree
+from mbff.structures.ADTree import ADTree, ADNode, VaryNode
 
 from mbff_tests.TestBase import TestBase
 
 class TestADTree(TestBase):
 
-    def test_simple_ADTree(self):
-        dataset = self.default_small_dataset()
-        dataset.finalize()
-        adtree = ADTree(dataset)
+    def assertVaryNodeCorrect(self, vary, column_index, values, mcv, children_count):
+        self.assertIsNotNone(vary)
+        self.assertEqual(column_index, vary.column_index)
+        self.assertEqual(values, vary.values)
+        self.assertEqual(mcv, vary.most_common_value)
+        self.assertEqual(children_count, len(vary.AD_children))
 
-        self.assertIsNotNone(adtree.Root)
-        self.assertEqual(8, adtree.Root.count)
-        self.assertEqual(2, len(adtree.Root.Vary_children))
 
-        vary1 = adtree.Root.Vary_children[0]
-        self.assertEqual(0, vary1.variableID)
-        self.assertEqual([1, 2, 3], vary1.values)
-        self.assertEqual(3, vary1.most_common_value)
-        self.assertEqual(3, len(vary1.AD_children))
+    def assertADNodeCorrect(self, adNode, column_index, value, count, children_count):
+        self.assertIsNotNone(adNode)
+        self.assertEqual(column_index, adNode.column_index)
+        self.assertEqual(value, adNode.value)
+        self.assertEqual(count, adNode.count)
+        self.assertEqual(children_count, len(adNode.Vary_children))
+
+
+    def test_simple_ADTree_query(self):
+        (dataset, column_values) = self.default_small_dataset()
+        adtree = ADTree(dataset, column_values)
+        
+        self.assertEqual(1/1, adtree.query({}))
+
+        self.assertEqual(1/8, adtree.query({0:1}))
+        self.assertEqual(3/8, adtree.query({0:2}))
+        self.assertEqual(4/8, adtree.query({0:3}))
+
+        self.assertEqual(0/8, adtree.query({0:1, 1:1}))
+        self.assertEqual(1/8, adtree.query({0:1, 1:2}))
+
+        self.assertEqual(2/8, adtree.query({0:2, 1:1}))
+        self.assertEqual(1/8, adtree.query({0:2, 1:2}))
+
+        self.assertEqual(1/8, adtree.query({0:3, 1:1}))
+        self.assertEqual(3/8, adtree.query({0:3, 1:2}))
+
+        self.assertEqual(3/8, adtree.query({1:1}))
+        self.assertEqual(5/8, adtree.query({1:2}))
+
+        self.assertEqual(0/1, adtree.query({1:1}, given={0:1}))
+        self.assertEqual(1/1, adtree.query({1:2}, given={0:1}))
+
+        self.assertEqual(2/3, adtree.query({1:1}, given={0:2}))
+        self.assertEqual(1/3, adtree.query({1:2}, given={0:2}))
+
+        self.assertEqual(1/4, adtree.query({1:1}, given={0:3}))
+        self.assertEqual(3/4, adtree.query({1:2}, given={0:3}))
+
+
+    def test_simple_ADTree_structure(self):
+        (dataset, column_values) = self.default_small_dataset()
+        adtree = ADTree(dataset, column_values)
+
+        self.assertIsNotNone(adtree.root)
+        self.assertEqual(8, adtree.root.count)
+        self.assertEqual(2, len(adtree.root.Vary_children))
+
+        vary1 = adtree.root.Vary_children[0]
+        self.assertVaryNodeCorrect(vary1, 0, [1, 2, 3], 3, 3)
 
         child0 = vary1.AD_children[0]
-        self.assertIsNotNone(child0)
-        self.assertEqual(1, child0.value)
-        self.assertEqual(1, child0.count)
-        self.assertEqual(1, len(child0.Vary_children))
+        self.assertADNodeCorrect(child0, 0, 1, 1, 1)
 
         child1 = vary1.AD_children[1]
-        self.assertIsNotNone(child1)
-        self.assertEqual(2, child1.value)
-        self.assertEqual(3, child1.count)
-        self.assertEqual(1, len(child1.Vary_children))
+        self.assertADNodeCorrect(child1, 0, 2, 3, 1)
 
         child2 = vary1.AD_children[2]
         self.assertIsNone(child2)
 
-        vary2 = adtree.Root.Vary_children[1]
-        self.assertEqual(1, vary1.variableID)
-        self.assertEqual([1, 2], vary1.values)
-        self.assertEqual(2, vary1.most_common_value)
-        self.assertEqual(2, len(vary1.AD_children))
+        vary2 = adtree.root.Vary_children[1]
+        self.assertVaryNodeCorrect(vary2, 1, [1, 2], 2, 2)
 
         child0 = vary2.AD_children[0]
-        self.assertIsNotNone(child0)
-        self.assertEqual(1, child0.value)
-        self.assertEqual(3, child0.count)
-        self.assertEqual(0, len(child0.Vary_children))
+        self.assertADNodeCorrect(child0, 1, 1, 3, 0)
 
         child1 = vary2.AD_children[1]
         self.assertIsNone(child1)
 
-        vary3 = adtree.Root.Vary_children[0].AD_children[0].Vary_children[0]
-        self.assertEqual(2, vary3.variableID)
-        self.assertEqual(2, vary3.most_common_value)
-        self.assertEqual([None, None], vary.AD_children)
+        vary3 = adtree.root.Vary_children[0].AD_children[0].Vary_children[0]
+        self.assertVaryNodeCorrect(vary3, 1, [1, 2], 2, 2)
+        self.assertEqual([None, None], vary3.AD_children)
 
-        vary4 = adtree.Root.Vary_children[0].AD_children[1].Vary_children[0]
-        self.assertEqual(2, vary4.variableID)
-        self.assertEqual(1, vary4.most_common_value)
-        self.assertEqual(2, len(vary.AD_children))
+        vary4 = adtree.root.Vary_children[0].AD_children[1].Vary_children[0]
+        self.assertVaryNodeCorrect(vary4, 1, [1, 2], 1, 2)
 
         child0 = vary4.AD_children[0]
         self.assertIsNone(child0)
 
         child1 = vary4.AD_children[1]
-        self.assertIsNotNone(child1)
-        self.assertEqual(2, child1.value)
-        self.assertEqual(1, child1.count)
-        self.assertEqual(0, len(child1.Vary_children))
+        self.assertADNodeCorrect(child1, 1, 2, 1, 0)
+
+
+    def test_simple_ADTree2_structure(self):
+        (dataset, column_values) = self.default_small_dataset_2()
+        adtree = ADTree(dataset, column_values)
+
+        self.assertIsNotNone(adtree.root)
+        self.assertEqual(16, adtree.root.count)
+        self.assertEqual(3, len(adtree.root.Vary_children))
+
+        vary1 = adtree.root.Vary_children[0]
+        self.assertVaryNodeCorrect(vary1, 0, [1, 2], 1, 2)
+
+        child11 = vary1.AD_children[0]
+        self.assertIsNone(child11)
+
+        child12 = vary1.AD_children[1]
+        self.assertADNodeCorrect(child12, 0, 2, 8, 2) 
+
+        vary121 = child12.Vary_children[0]
+        self.assertVaryNodeCorrect(vary121, 1, [1, 2, 3, 4], 1, 4)
+
+        child1211 = vary121.AD_children[0]
+        self.assertIsNone(child1211)
+
+        child1212 = vary121.AD_children[1]
+        self.assertADNodeCorrect(child1212, 1, 2, 2, 1)
+        vary12121 = child1212.Vary_children[0]
+        self.assertVaryNodeCorrect(vary12121, 2, [1, 2], 1, 2)
+        child121211 = vary12121.AD_children[0]
+        self.assertIsNone(child121211)
+        child121212 = vary12121.AD_children[1]
+        self.assertADNodeCorrect(child121212, 2, 2, 1, 0)
+
+        child1213 = vary121.AD_children[2]
+        self.assertADNodeCorrect(child1213, 1, 3, 2, 1)
+        vary12131 = child1213.Vary_children[0]
+        self.assertVaryNodeCorrect(vary12131, 2, [1, 2], 1, 2)
+        child121311 = vary12131.AD_children[0]
+        self.assertIsNone(child121311)
+        child121312 = vary12131.AD_children[1]
+        self.assertADNodeCorrect(child121312, 2, 2, 1, 0)
+
+        child1214 = vary121.AD_children[3]
+        self.assertADNodeCorrect(child1214, 1, 4, 2, 1)
+        vary12141 = child1214.Vary_children[0]
+        self.assertVaryNodeCorrect(vary12141, 2, [1, 2], 1, 2)
+        child121411 = vary12141.AD_children[0]
+        self.assertIsNone(child121411)
+        child121412 = vary12141.AD_children[1]
+        self.assertADNodeCorrect(child121412, 2, 2, 1, 0)
+
+        vary122 = child12.Vary_children[1]
+        self.assertVaryNodeCorrect(vary122, 2, [1, 2], 1, 2)
+        child1221 = vary122.AD_children[0]
+        self.assertIsNone(child1221)
+        child1222 = vary122.AD_children[1]
+        self.assertADNodeCorrect(child1222, 2, 2, 4, 0)
+
+        vary2 = adtree.root.Vary_children[1]
+        self.assertVaryNodeCorrect(vary2, 1, [1, 2, 3, 4], 1, 4)
+        child21 = vary2.AD_children[0]
+        self.assertIsNone(child21)
+        child22 = vary2.AD_children[1]
+        self.assertADNodeCorrect(child22, 1, 2, 4, 1)
+        vary221 = child22.Vary_children[0]
+        self.assertVaryNodeCorrect(vary221, 2, [1, 2], 1, 2)
+        child2211 = vary221.AD_children[0]
+        self.assertIsNone(child2211)
+        child2212 = vary221.AD_children[1]
+        self.assertADNodeCorrect(child2212, 2, 2, 2, 0)
+
+        child23 = vary2.AD_children[2]
+        self.assertADNodeCorrect(child23, 1, 3, 4, 1)
+        vary231 = child23.Vary_children[0]
+        self.assertVaryNodeCorrect(vary231, 2, [1, 2], 1, 2)
+        child2311 = vary231.AD_children[0]
+        self.assertIsNone(child2311)
+        child2312 = vary231.AD_children[1]
+        self.assertADNodeCorrect(child2312, 2, 2, 2, 0)
+        
+        child24 = vary2.AD_children[3]
+        self.assertADNodeCorrect(child24, 1, 4, 4, 1)
+        vary241 = child24.Vary_children[0]
+        self.assertVaryNodeCorrect(vary241, 2, [1, 2], 1, 2)
+        child2411 = vary241.AD_children[0]
+        self.assertIsNone(child2411)
+        child2412 = vary241.AD_children[1]
+        self.assertADNodeCorrect(child2412, 2, 2, 2, 0)
+
+        vary3 = adtree.root.Vary_children[2]
+        self.assertVaryNodeCorrect(vary3, 2, [1, 2], 1, 2)
+        child31 = vary3.AD_children[0]
+        self.assertIsNone(child31)
+        child32 = vary3.AD_children[1]
+        self.assertADNodeCorrect(child32, 2, 2, 8, 0)
+
+
 
 
     def default_small_dataset(self):
-        datasetmatrix = DatasetMatrix('test_small')
-        datasetmatrix.X = scipy.sparse.csr_matrix(numpy.array([
-            [1, 2, 2, 2, 3, 3, 3, 3],
+        dataset = scipy.sparse.csr_matrix(numpy.array([
+            [1, 2, 3, 2, 2, 3, 3, 3],
             [2, 1, 1, 2, 1, 2, 2, 2]]).transpose())
-        datasetmatrix.Y = scipy.sparse.csr_matrix(numpy.empty((8, 2)))
-        datasetmatrix.row_labels = ['row{}'.format(i) for i in range(0, 8)]
-        datasetmatrix.column_labels_X = ['a1', 'a2']
-        datasetmatrix.column_labels_Y = []
 
-        return datasetmatrix
+        column_values = {
+                0: [1, 2, 3],
+                1: [1, 2]}
+        return (dataset, column_values)
+
+
+    def default_small_dataset_2(self):
+        dataset = scipy.sparse.csr_matrix(numpy.array([
+            [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2],
+            [1, 1, 2, 2, 3, 3, 4, 4, 1, 1, 2, 2, 3, 3, 4, 4],
+            [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2]]).transpose())
+
+        column_values = {
+                0: [1, 2],
+                1: [1, 2, 3, 4],
+                2: [1, 2]}
+        return (dataset, column_values)
