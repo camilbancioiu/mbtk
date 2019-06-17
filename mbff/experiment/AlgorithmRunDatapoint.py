8aa3a909d9fde19881a40ed6db2f9290a1a6ae97
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2019-06-17 18:14:24 +0300

    Ensure that the parameters received by an AlgorithmRunDatapoint can be pickled

--- mbff/experiment/AlgorithmRunDatapoint.py
+++ mbff/experiment/AlgorithmRunDatapoint.py
@@ -1,6 +1,6 @@
 class AlgorithmRunDatapoint:
     def __init__(self, algorithm_run):
-        self.parameters = algorithm_run.parameters
+        self.parameters = self.pickleable_parameters(algorithm_run.parameters)
         self.label = algorithm_run.label
         self.ID = algorithm_run.ID
         self.algorithm_name = algorithm_run.algorithm_name
@@ -8,6 +8,13 @@ class AlgorithmRunDatapoint:
         self.start_time = algorithm_run.start_time
         self.end_time = algorithm_run.end_time
         self.duration = algorithm_run.duration
+
+
+    def pickleable_parameters(self, parameters):
+        newparameters = parameters.copy()
+        for k, v in newparameters.items():
+            if callable(v):
+                newparameters[k] = str(v)
 
 
 
