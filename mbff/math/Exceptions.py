57fe41752f6d6aa4f456fe80b1ed8ff99c2d86d4
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2019-06-26 19:13:30 +0300

    Improve string representation of the exception VariableInstancesOfUnequalCount

--- mbff/math/Exceptions.py
+++ mbff/math/Exceptions.py
@@ -21,5 +21,9 @@ class VariableInstancesOfUnequalCount(Exception):
     def __init__(self, variables):
         self.variables = variables
         self.variableIDs = [var.ID for var in self.variables]
-        self.variableEnum = ", ".join(["{} ({})".format(var.ID, var.name) for var in variables])
+        self.variableEnum = ", ".join(["ID {} (named {}, length {})".format(var.ID, var.name, len(var.instances)) for var in variables])
         self.message = "Variables {} have unequal numbers of instances".format(self.variableEnum)
+
+
+    def __str__(self):
+        return self.message
