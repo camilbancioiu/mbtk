5cb4dc1fcda6233f87ab4e50a0c469272e8e8f6e
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2020-06-07 18:59:49 +0300

    Fix experiment script

--- experiments/experiment_dcmi/experiment_dcmi.sh
+++ experiments/experiment_dcmi/experiment_dcmi.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
 
-expshell="py main.py"
+expshell="python3 main.py"
 
 function build_adtrees() {
     local dataset_name=$1
     local sample_count=$2
     local tree_type=$3
-    local experiment=$expshell $dataset_name $sample_count
+    local experiment="$expshell $dataset_name $sample_count"
 
     echo "Building AD-tree, pruning 0"
     $experiment adtree build --tree-type=$tree_type --llt=0
 
     echo "Building AD-tree, pruning 5"
-    $experiment --llt 5 adtree build --tree-type=$tree_type --llt=5
+    $experiment adtree build --tree-type=$tree_type --llt=5
 
     echo "Building AD-tree, pruning 10"
-    $experiment --llt 10 adtree build --tree-type=$tree_type --llt=10
+    $experiment adtree build --tree-type=$tree_type --llt=10
 }
 
 function run_unoptimized() {
     local dataset_name=$1
     local sample_count=$2
-    local experiment=$expshell $dataset_name $sample_count
+    local experiment="$expshell $dataset_name $sample_count"
     $experiment exp unlock
     $experiment --algrun-tag=unoptimized exp run
 }
@@ -29,7 +29,7 @@ function run_unoptimized() {
 function run_dcmi() {
     local dataset_name=$1
     local sample_count=$2
-    local experiment=$expshell $dataset_name $sample_count
+    local experiment="$expshell $dataset_name $sample_count"
     $experiment exp unlock
     $experiment --algrun-tag=dcmi exp run
 }
@@ -39,7 +39,7 @@ function run_adtree() {
     local sample_count=$2
     local tree_type=$3
     local llt=$4
-    local experiment=$expshell $dataset_name $sample_count
+    local experiment="$expshell $dataset_name $sample_count"
     $experiment exp unlock
     $experiment --algrun-tag="adtree-$tree_type-llt$llt" exp run
 }
@@ -47,7 +47,7 @@ function run_adtree() {
 function fullexperiment() {
     local dataset_name=$1
     local sample_count=$2
-    local experiment=$expshell $dataset_name $sample_count
+    local experiment="$expshell $dataset_name $sample_count"
 
     echo "=========================================="
     echo "Running full experiment for dataset $dataset_name and sample size $sample_count"
@@ -68,3 +68,5 @@ function fullexperiment() {
     run_adtree $dataset_name $sample_count dynamic 5
     run_adtree $dataset_name $sample_count dynamic 10
 }
+
+fullexperiment "$@"
